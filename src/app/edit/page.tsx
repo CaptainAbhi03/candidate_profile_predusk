4124e3f12da28ada70befe6622302747dcdc2585
Abhineet Mathur <144213055+CaptainAbhi03@users.noreply.github.com>
2025-09-02 03:11:06 +0530

    Update page.tsx

--- src/app/edit/page.tsx
+++ src/app/edit/page.tsx
@@ -14,6 +14,7 @@ import { useToast } from '@/hooks/use-toast';
 import { ProfileSchema } from '@/types/zod-schema';
 import type { Profile } from '@/types';
 import { Trash2, PlusCircle, Loader2 } from 'lucide-react';
+import Link from 'next/link';
 
 export default function EditProfilePage() {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
